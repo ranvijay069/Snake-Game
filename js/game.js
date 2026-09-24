6f5e6341a16f89c0b88ec767d6ef9cd97bec4f49
Ranvijay Kumar <iranvijay.official005@gmail.com>
2026-09-24 10:42:47 +0530

    Initial commit - Snake Game

---
+++ js/game.js
@@ -0,0 +1,18 @@
+import { Snake } from './snake.js';
+
+export class Game {
+
+    constructor() {
+
+        this.rows = 20;
+
+        this.columns = 20;
+
+        //create snake 
+        this.snake = new Snake();
+
+        //craete food
+
+    }
+
+}
---
+++ js/snake.js
@@ -0,0 +1,17 @@
+export class Snake {
+
+    constructor() {
+        this.body = [
+            { x: 10, y: 10 },
+            { x: 9, y: 10 },
+            { x: 8, y: 10 },
+        ];
+    }
+    getBody() {
+
+        return this.body;
+
+    }
+
+
+}
